72f7e1c82668c4a5d9f6a97951c43ef0a0fc1bf6
joao barbirato <joaobarbirato@gmail.com>
2018-11-09 01:03:17 -0200

    Adicionados EJS auxiliares para criação de páginas

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
